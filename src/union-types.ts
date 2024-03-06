01679ccf400ea045c1b0b4acfccbc3f43ee04a8b
Stanislau Ihnatsenka <stanislau.ihnatsenka@hiqo-solutions.com>
2024-03-06 12:22:50 +0100

    add union-types example

---
+++ src/union-types.ts
@@ -0,0 +1,5 @@
+type NewType = string | number;
+
+const firstExample: NewType = 1;
+const secondExample: NewType = '1';
+// const thirdExample: NewType = true; Type 'boolean' is not assignable to type 'NewType'.ts(2322)
