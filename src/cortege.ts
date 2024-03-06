6331fc67a64d7d063f7939b1493ce5520e07128f
Stanislau Ihnatsenka <stanislau.ihnatsenka@hiqo-solutions.com>
2024-03-06 16:03:45 +0100

    fix cortege example

--- src/cortege.ts
+++ src/cortege.ts
@@ -1,4 +1,4 @@
 // CORTEGE - is an array with a fixed size and a known set of data
 
-let person: [string, number, boolean] = ['Stas', 1, true];
-// person[0] = 100; // Error
+let array: [string, number, boolean] = ['Stas', 1, true];
+// array[0] = 100; // Type 'number' is not assignable to type 'string'.ts(2322)
