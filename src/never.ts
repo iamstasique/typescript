05964c8d9c85f9e587d45e709685c9a0c246161d
Stanislau Ihnatsenka <stanislau.ihnatsenka@hiqo-solutions.com>
2024-03-08 12:41:33 +0100

    add new never examples

--- src/never.ts
+++ src/never.ts
@@ -1,5 +1,9 @@
 // you can assign NEVER to a variable, but you cannot assign a variable to NEVER
 
+
+/**
+ * FIRST example
+ */
 let neverVariable: never;
 let stringVariable: string;
 let numberVariable: number;
@@ -30,3 +34,58 @@ function exampleWithNever_2(key: 'a' | 'b') {
       return key; // (parameter) key: never
   }
 }
+
+/**
+ * SECOND example
+ */
+
+enum ActionTypes {
+  Intement = 'increment',
+  Decrement = 'decrement',
+}
+
+interface State {
+  count: number;
+}
+type SomeAction = { type: ActionTypes.Intement; payload: number } | { type: ActionTypes.Decrement; payload: number };
+
+function someMethod(state: State, action: SomeAction): State {
+  const { type, payload } = action;
+
+  switch (type) {
+    case ActionTypes.Intement: {
+      return { ...state, count: state.count + payload };
+    }
+    case ActionTypes.Decrement: {
+      return { ...state, count: state.count - payload };
+    }
+    default:
+      return methodWithNeverArgument(type); // will work
+  }
+}
+
+function someMethod2(state: State, action: SomeAction) {
+  const { type, payload } = action;
+
+  switch (type) {
+    case ActionTypes.Intement: {
+      return { ...state, count: state.count + payload };
+    }
+    default:
+    //   return methodWithNeverArgument(type);
+    // Argument of type 'ActionTypes' is not assignable to parameter of type 'never'.ts(2345) - because we didn't check "ActionTypes.Decrement"
+  }
+}
+
+function methodWithNeverArgument(value: never): never {
+  return value;
+}
+
+
+/**
+ * THIRD example
+ */
+
+type FilterTestType<T extends string> = T extends 'trash' ? never : T;
+type TestType = FilterTestType<'hello' | 'world' | 'how' | 'are' | 'you' | 'trash'>
+// result is - type TestType = "hello" | "world" | "how" | "are" | "you" - because we filtered out type
